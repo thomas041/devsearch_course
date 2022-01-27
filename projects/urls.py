de2f1e23d0cd6f1ada60cb79ae66bdf35da860a9
thomas041 <41703828+thomas041@users.noreply.github.com>
2022-01-27 17:57:38 +0000

    Initial Commit

---
+++ projects/urls.py
@@ -0,0 +1,11 @@
+from django.contrib import admin
+from django.urls import path
+from . import views
+
+
+urlpatterns = [
+
+    path('', views.projects, name="projects"),
+    path('project/<str:pk>/', views.project, name="project"),
+
+]
