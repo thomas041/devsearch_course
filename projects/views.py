3b0126bbe183bfdccb21118e67fe5585e914ac14
tom <tom@morgz.co.uk>
2022-02-17 17:38:40 +0000

    Late commit

--- projects/views.py
+++ projects/views.py
@@ -1,33 +1,91 @@
-from django.shortcuts import render
+from django.core import paginator
+from django.shortcuts import render, redirect
 from django.http import HttpResponse
+from django.contrib.auth.decorators import login_required
+from django.contrib import messages
+from .models import Project, Tag
+from .forms import ProjectForm, ReviewForm
+from .utils import searchProjects, paginateProjects
 
 
 def projects(request):
-    page = 'projects'
-    number = 10
-    context = {'page': page, 'number': number, 'projects': projectsList}
+    projects, search_query = searchProjects(request)
+    custom_range, projects = paginateProjects(request, projects, 6)
+
+    context = {'projects': projects,
+               'search_query': search_query, 'custom_range': custom_range}
     return render(request, 'projects/projects.html', context)
 
 
 def project(request, pk):
-    projectObj = None
-    for i in projectsList:
-        if i['id'] == pk:
-            projectObj = i
-    return render(request, 'projects/single-project.html', {'project': projectObj})
+    projectObj = Project.objects.get(id=pk)
+    form = ReviewForm()
+
+    if request.method == 'POST':
+        form = ReviewForm(request.POST)
+        review = form.save(commit=False)
+        review.project = projectObj
+        review.owner = request.user.profile
+        review.save()
+
+        projectObj.getVoteCount
+
+        messages.success(request, 'Your review was successfully submitted!')
+        return redirect('project', pk=projectObj.id)
+
+    return render(request, 'projects/single-project.html', {'project': projectObj, 'form': form})
 
 
-projectsList = [
+@login_required(login_url="login")
+def createProject(request):
+    profile = request.user.profile
+    form = ProjectForm()
 
-            {'id': '1',
-             'title': 'Ecommerce Website',
-             'description': 'Fully functional ecommerce website'},
+    if request.method == 'POST':
+        newtags = request.POST.get('newtags').replace(',',  " ").split()
+        form = ProjectForm(request.POST, request.FILES)
+        if form.is_valid():
+            project = form.save(commit=False)
+            project.owner = profile
+            project.save()
 
-            {'id': '2',
-             'title': 'Portfolio Website',
-             'description': 'A personal website to write articles and display work'},
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
+            return redirect('account')
 
-            {'id': '3',
-             'title': 'Social Network',
-             'description': 'An open source project built by the community'}
-]
+    context = {'form': form}
+    return render(request, "projects/project_form.html", context)
+
+
+@login_required(login_url="login")
+def updateProject(request, pk):
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
+    form = ProjectForm(instance=project)
+
+    if request.method == 'POST':
+        newtags = request.POST.get('newtags').replace(',',  " ").split()
+
+        form = ProjectForm(request.POST, request.FILES, instance=project)
+        if form.is_valid():
+            project = form.save()
+            for tag in newtags:
+                tag, created = Tag.objects.get_or_create(name=tag)
+                project.tags.add(tag)
+
+            return redirect('account')
+
+    context = {'form': form, 'project': project}
+    return render(request, "projects/project_form.html", context)
+
+
+@login_required(login_url="login")
+def deleteProject(request, pk):
+    profile = request.user.profile
+    project = profile.project_set.get(id=pk)
+    if request.method == 'POST':
+        project.delete()
+        return redirect('projects')
+    context = {'object': project}
+    return render(request, 'delete_template.html', context)
